ed890b0fe163ffd954722d880f0319e14b808019
yuki-zmstr <yukihide99@gmail.com>
2022-12-28 14:02:45 +0800

    added preload message to Choose Prefectures

--- src/components/ChoosePrefectures.jsx
+++ src/components/ChoosePrefectures.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import regionData from '../utils/regionData';
 
 import styles from '../stylesheets/ChoosePrefectures.module.css';
 
 const ChoosePrefectures = () => {
+  const [preLoadMessage, setPreLoadMessage] = useState('Loading prefectures...');
   return (
     <fieldset className={styles.fieldset}>
       <legend>都道府県</legend>
-      <div />
+      <div>
+        <p>{preLoadMessage}</p>
+        {/* {Object.keys(regionData).map((key) => (
+          <div key={key}>
+            <h3 className={styles.region}>{key}</h3>
+            <div className={styles.prefectureSelections}>{filterPrefecture(key)}</div>
+          </div>
+        ))} */}
+      </div>
     </fieldset>
   );
 };

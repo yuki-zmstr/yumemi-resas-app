b33e1a2868ab842bc37639bc97d5539d078b4670
yuki-zmstr <yukihide99@gmail.com>
2022-12-28 13:56:55 +0800

    added AddPrefecture and RemovePrefecture handlers

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ChoosePrefectures from './ChoosePrefectures';
 import PopulationGraph from './PopulationGraph';
 
 const Body = () => {
+  const [selections, setSelections] = useState([]);
+
+  const addPrefectureHandler = (prefCode) => {
+    setSelections((prevSelections) => [...prevSelections, prefCode]);
+  };
+
+  const removePrefectureHandler = (prefCode) => {
+    setSelections((prevSelections) => {
+      return prevSelections.filter((prefecture) => prefecture !== prefCode);
+    });
+  };
+
   return (
     <>
-      <ChoosePrefectures />
+      <ChoosePrefectures
+        onAddPrefecture={(prefCode) => addPrefectureHandler(prefCode)}
+        onRemovePrefecture={(prefCode) => removePrefectureHandler(prefCode)}
+      />
       <PopulationGraph />
     </>
   );

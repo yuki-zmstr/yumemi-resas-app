04861c2e3491efcfcb3ac4b61472dd2db0921224
yuki-zmstr <yukihide99@gmail.com>
2022-12-28 14:16:12 +0800

    added container for LineChart

--- src/components/PopulationGraph.jsx
+++ src/components/PopulationGraph.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const PopulationGraph = () => {
-  return <div />;
+import styles from '../stylesheets/PopulationGraph.module.css';
+
+const PopulationGraph = ({ result, message }) => {
+  return (
+    <div className={styles.container}>
+      <p>{message}</p>
+      <h3>
+        最終更新日:
+        {new Date().toLocaleString('en-US', { timeZone: 'Asia/Tokyo' }).slice(0, 10)}
+      </h3>
+
+      <div data-testid='line-chart' className={styles.lineChart} />
+    </div>
+  );
+};
+
+PopulationGraph.propTypes = {
+  result: PropTypes.instanceOf(Array),
+  message: PropTypes.string,
+};
+
+PopulationGraph.defaultProps = {
+  result: [],
+  message: '',
 };
 
 export default PopulationGraph;

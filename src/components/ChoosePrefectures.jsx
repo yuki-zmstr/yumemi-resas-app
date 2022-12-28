fbdb7a1d1ed72b5d09408a22955b7a0467dc605e
yuki-zmstr <yukihide99@gmail.com>
2022-12-28 14:00:48 +0800

    added basic fieldset to Choose Prefectures

--- src/components/ChoosePrefectures.jsx
+++ src/components/ChoosePrefectures.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import styles from '../stylesheets/ChoosePrefectures.module.css';
+
 const ChoosePrefectures = () => {
-  return <div />;
+  return (
+    <fieldset className={styles.fieldset}>
+      <legend>都道府県</legend>
+      <div />
+    </fieldset>
+  );
 };
 
 export default ChoosePrefectures;

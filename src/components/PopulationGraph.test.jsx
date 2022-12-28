80ed73ccf0f70684878bbf613d8c4fd80bc0edc2
yuki-zmstr <yukihide99@gmail.com>
2022-12-28 14:41:30 +0800

    Added test code for PopulationGraph.jsx

---
+++ src/components/PopulationGraph.test.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import PopulationGraph from './PopulationGraph';
+
+jest.mock('recharts', () => ({
+  ...jest.requireActual('recharts'),
+  ResponsiveContainer: (props) => <div {...props} />,
+}));
+
+describe('PopulationGraph', () => {
+  it('checks the value of last updated date', () => {
+    const { getByText } = render(<PopulationGraph />);
+    const today = new Date().toLocaleString('en-US', { timeZone: 'Asia/Tokyo' }).slice(0, 10);
+    const h3value = getByText(`最終更新日:${today}`);
+    expect(h3value).toBeInTheDocument();
+  });
+
+  it('checks existance of line-chart', () => {
+    const { queryByTestId } = render(<PopulationGraph />);
+    const lineChart = queryByTestId('line-chart');
+    expect(lineChart).toBeInTheDocument();
+  });
+});

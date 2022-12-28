cb72675fb5a7f7e057735d3f737cbf4d51f1c689
yuki-zmstr <yukihide99@gmail.com>
2022-12-28 13:51:18 +0800

    added functions for getting prefectures and getting population data

---
+++ src/api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://opendata.resas-portal.go.jp',
+  headers: {
+    'content-type': 'application/json;charset=UTF-8',
+    'X-API-KEY': process.env.REACT_APP_API_KEY,
+  },
+});
+
+export function getPrefectures() {
+  return instance({
+    method: 'GET',
+    url: '/api/v1/prefectures',
+  });
+}
+
+export function getPopulationData(prefcode) {
+  return instance({
+    method: 'GET',
+    url: 'api/v1/population/composition/perYear',
+    params: {
+      prefCode: prefcode,
+      cityCode: '-',
+    },
+  });
+}
